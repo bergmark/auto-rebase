115a6e40c8930e8d221812accf188fff2ff7101c
Scott Steele <scottlsteele@gmail.com>
2023-01-03 18:28:49 -0500

    squash!
    tinkering to be able to dev

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,18 @@ import {
     getInput,
     setFailed,
 } from "@actions/core";
-import { context } from "@actions/github";
+// import { context } from "@actions/github";
+const context_raw = getInput('context');
+console.log({ context_raw });
+const context = JSON.parse(context_raw);
 import { Octokit } from "@octokit/rest";
 
 const { rebasePullRequest } = require('github-rebase');
 
-const token = getInput('github_token')
-const filter = getInput('filter')
+const token = getInput('github_token');
+const filter = getInput('filter');
+// TODO: better error on failed parse
+const max_mergeable_rebases = parseInt(getInput('max_mergeable_rebases'));
 if (!['always', 'auto-merge'].includes(filter)) {
     setFailed("Illegal filter used");
 }
@@ -23,18 +28,21 @@ console.log(`Repository: ${repo}`)
 console.log(`Current branch: ${base}`)
 
 try {
-    run(octokit, owner, repo, base);
+    // run(octokit, owner, repo, base);
+    getPullsToRebase(octokit, owner, repo, base).then(console.log);
 } catch(error: any) {
     setFailed(error.message);
 }
 
-async function run(octokit: Octokit, owner: string, repo: string, base: string) {
-    const pulls = await octokit.paginate("GET /repos/{owner}/{repo}/pulls", {
-        owner: owner,
-        repo: repo,
-        base: base
+async function getPullsToRebase(octokit: Octokit, owner: string, repo: string, base: string) {
+    const url = "/repos/{owner}/{repo}/pulls";
+    console.log({ url });
+    const pulls = await octokit.paginate(`GET ${url}`, {
+        owner,
+        repo,
+        base
     }, res => res.data);
-    
+
     let pullsToRebase;
     if (filter === 'auto-merge') {
         pullsToRebase = pulls.filter(pull => pull.auto_merge !== null)
@@ -45,6 +53,12 @@ async function run(octokit: Octokit, owner: string, repo: string, base: string) 
     } else {
         pullsToRebase = pulls
     }
+
+    return pullsToRebase
+}
+
+async function run(octokit: Octokit, owner: string, repo: string, base: string) {
+    let pullsToRebase = await getPullsToRebase(octokit, owner, repo, base)
 
     await Promise.all(pullsToRebase.map(async (pull) => {
         try {
